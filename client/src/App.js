ae14b93d33b38a400aacccce0542b23c5ce7c4d7
Anita Angelidou <anitawebdesigns@gmail.com>
2023-02-11 14:13:01 +0200

    added backend for user register, login and verification with token

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 import { useState, useEffect } from "react";
 import Message from "./components/Message";
 import Rooms from "./components/Rooms";
@@ -21,8 +21,8 @@ function App() {
 
   // send a message throught the socket to the back end with the room number the user entered
   const joinRoom = () => {
-    if(room !== ""){
-    socket.emit("join_room", room );
+    if (room !== "") {
+      socket.emit("join_room", room);
     }
   };
 
@@ -32,24 +32,26 @@ function App() {
   };
 
   return (
-    <div style={{ width: "50%", margin: "3rem auto" }}>
-      <Rooms
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-        onClick={() => {
-          joinRoom();
-        }}
-      />
-      <Message
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-        onClick={() => {
-          sendMessage();
-        }}
-        children={<p>{messageReceived}</p>}
-      />
+    <div>
+      <div style={{ width: "50%", margin: "3rem auto" }}>
+        <Rooms
+          onChange={(event) => {
+            setRoom(event.target.value);
+          }}
+          onClick={() => {
+            joinRoom();
+          }}
+        />
+        <Message
+          onChange={(event) => {
+            setMessage(event.target.value);
+          }}
+          onClick={() => {
+            sendMessage();
+          }}
+          children={<p>{messageReceived}</p>}
+        />
+      </div>
     </div>
   );
 }
