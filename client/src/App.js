3278182496c59e6bb10343e1109d3bfacc386aac
Anita Angelidou <anitawebdesigns@gmail.com>
2023-02-12 18:43:27 +0200

    refactored authController and added login authentication based on username or password

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
+import Login from './pages/Login'
 import ChatRoom from './pages/ChatRoom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/register" element={<SignUp/>} />
+            <Route path="/login" element={<Login/>} />
             <Route path="/chat" element={<ChatRoom />} />
           </Routes>
         </div>
