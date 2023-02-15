a8aeddcd4306ef41a8c9ae184253d5862b2b7b26
Anita Angelidou <anitawebdesigns@gmail.com>
2023-02-15 18:34:01 +0200

    created socket.js for frontend conncectivity to socket.io & testing socket seems to work this time

---
+++ server/modules/chatModule.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ChatSchema = new Schema({
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  receiver: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Chat = mongoose.model("Chat", ChatSchema);
