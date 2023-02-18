df504f8a4355f8de2e7e079c01645a002baa25b1
Anita Angelidou <anitawebdesigns@gmail.com>
2023-02-18 13:39:56 +0200

    displayed old messages and message Received, but grew out of container

--- client/src/pages/ChatRoom.js
+++ client/src/pages/ChatRoom.js
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import { io, Socket } from "socket.io-client";
-import axios from 'axios';
+import * as React from "react";
+import axios from "axios";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Sidebar from "../components/Sidebar";
 import MessageBoard from "../components/MessageForm";
-import { Grid, Container, Box } from "@mui/material";
+import { Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { socketID, socket } from "../Socket";
 
@@ -14,96 +14,126 @@ function App() {
     setMessageReceived(`You connected with id ${socketID}`);
   });
 
-  // NavBar Variables
+  // -------- VARIABLES -------
+
+  const navigate = useNavigate();
+
+  // -- NavBar Variables --
   let pages = [
     { text: "Chat", href: "/chat" },
     { text: "Login", href: "/login" },
   ];
-  
   let settings = ["Profile", "Account", "Dashboard", "Logout"];
 
-  // Room Variables
+  // -- Room Variables --
   const [room, setRoom] = useState("");
 
   const handleListItemClick = (event, idx, chatroom) => {
-    setRoom(room)
+    setRoom(room);
     console.log(room);
   };
 
-
-  // Chat Variables
+  // -- Chat Variables --
+  const [chat, setChat] = useState([]);
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
   // const [room, setRoom] = useState("");
   const [connectedUser, setConnectedUser] = useState({
+    id: "",
     username: "",
-    imageUrl: "",
+    // picture: "",
   });
 
-  // log authenticated users
-  // useEffect(()=>{
-  //   if (localStorage.getItem("token")) {
-  //     axios
-  //       .post("http://localhost:5000/auth/verify", {
-  //         token: localStorage.getItem("token"),
-  //       })
-  //       .then(({ data }) => {
-  //         setConnectedUser(data);
-  //         console.log(connectedUser)
-  //       });
-  //   }
-  // }, []);
+  // -------- HOOKS  -------
+
+  // Show chat only to authenticated users
+  useEffect(() => {
+    // authenticate the user and get the token from localstorage
+    if (localStorage.getItem("token")) {
+      axios
+        .post("http://localhost:5000/auth/verify", {
+          token: localStorage.getItem("token"),
+        })
+        .then(({ data }) => {
+          console.log(data);
+          setConnectedUser({ id: data._id, username: data.username });
+        })
+        // get the messages for the authenticated user to display the chat
+        .then(
+          axios.get("http://localhost:5000/chat/")
+          .then(({data}) => {
+            setChat(data)
+          }))
+    // if the user is not authenticated redirect to home page
+    } else {
+     navigate('/');
+    }
+  }, []);
 
   // listen to any changes coming from the socket
-  useEffect(() => { 
-  socket.on(
-    "receive_message",
-    (data) => {
-      setMessageReceived(data.message);
-    },
-    [socket]
-  );
+  useEffect(() => {
+    socket.on(
+      "receive_message",
+      (data) => {
+        setMessageReceived(data.message);
+      },
+      [socket]
+    );
   });
 
-
+  // -------- FUNCTIONS  -------
 
   // send a message throught the socket to the back end with the room number the user entered
   const joinRoom = () => {
     if (room !== "") {
       // socket.emit("join_room", room);
-      alert(room)
+      alert(room);
     }
   };
 
   // send a message through the socket, once the user clicks the send button
   const sendMessage = () => {
-    axios.post('http://localhost:5000/post', {
-      message:"hi", 
-      sender: socketID,
-    })
-    .then(
-      socket.emit("send_message", {
+    axios
+      .post("http://localhost:5000/chat/post", {
         message,
-        // room
+        sender: connectedUser.id,
       })
-    )  
+      .then(
+        socket.emit("send_message", {
+          message,
+          // room
+        })
+      );
   };
 
-  
+  // -------- RETURN STATEMENT  -------
+
   return (
     <div>
       <NavBar pages={pages} settings={settings} />
       <Grid container spacing={2}>
         <Grid item xs={12} sm={4}>
-          <Sidebar         
-             />
+          <Sidebar />
         </Grid>
         <Grid item xs={12} sm={8}>
           <MessageBoard
             onClick={() => sendMessage()}
-            children={<Typography>{messageReceived}</Typography>}
+            children={
+            <Typography>
+              {chat.map((message) => {
+              
+                return (
+                  <>
+                <p>{message.message}</p>
+                
+             
+                </>
+                )
+              })}
+              {messageReceived}
+              </Typography>}
             onChange={(event) => {
-            setMessage(event.target.value); 
+              setMessage(event.target.value);
             }}
           />
           {/* <div>{messageReceived}</div>
@@ -111,6 +141,14 @@ function App() {
               setMessage(event.target.value);
             }}></input>
             <button onClick={() => sendMessage()}>Send</button> */}
+          <button
+            onClick={() => {
+              console.log(connectedUser, message);
+              console.log(chat);
+            }}
+          >
+            User
+          </button>
         </Grid>
       </Grid>
     </div>
