41c87977c4ba79aba8b58d227dde9443d3a08461
Anita Angelidou <anitawebdesigns@gmail.com>
2023-02-19 23:37:51 +0200

    displays messages in boxes of different colors, on the right for other users, on the left for current user

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,32 +1,56 @@
 import React from "react";
 import Moment from "react-moment";
-import { Typography, Box } from "@mui/material";
+import { Typography, Container, Box } from "@mui/material";
 
 function Message({ chat, messageReceived, connectedUser }) {
-
+  
 
   return (
       <Typography>
-              {chat.map((message) => {         
-            return (
-              <Box sx={{
-                display:"flex",
-                flexDirection: "column",
-                flexWrap: "wrap",
-                width: "20vw",
-                my:1,
-                ml:2,
-                p:1,
-                backgroundColor: 'lightblue',
-                borderRadius: "10px",
-              }}>
-              <p>{message?.sender?._id === connectedUser.id ? "You" : message?.sender?.username }</p>
-              <p>{message.message}</p>      
-              <p>{<Moment format="DD/MM/YYYY HH:mm">{message.createdAt}</Moment>}</p>
-             </Box >
-                )
+        <Container  sx={{ display: 'flex', flexDirection: 'column' }}>
+              {chat.map((message) => {   
+                if(message?.sender?._id === connectedUser.id){
+                  return (
+                    <Box sx={{
+                      display:"flex",
+                      flexDirection: "column",
+                      flexWrap: "wrap",
+                      width: "20vw",
+                      my:1,
+                      ml:2,
+                      p:1,
+                      backgroundColor: 'lightblue',
+                      borderRadius: "10px",
+                    }}>
+                    <p>"You"</p>
+                    <p>{message.message}</p>      
+                    <p>{<Moment format="DD/MM/YYYY HH:mm">{message.createdAt}</Moment>}</p>
+                   </Box >
+                      )
+                }   else {
+                  return (
+                    <Box sx={{
+                      display:"flex",
+                      flexDirection: "column",
+                      flexWrap: "wrap",
+                      alignSelf: "flex-end",
+                      width: "20vw",
+                      my:1,
+                      ml:2,
+                      p:1,
+                      backgroundColor: 'lightpink',
+                      borderRadius: "10px",
+                    }}>
+                    <p>{message?.sender?.username}</p>
+                    <p>{message.message}</p>      
+                    <p>{<Moment format="DD/MM/YYYY HH:mm">{message.createdAt}</Moment>}</p>
+                   </Box >
+                  )
+                }   
+            
               })}
-              {messageReceived}
+               </Container>
+              {messageReceived}            
               </Typography>
   );
 }
