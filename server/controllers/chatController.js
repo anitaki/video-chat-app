0fdd50df6fec644d1d7f02c4fcbe6ad2a704f936
Anita Angelidou <anitawebdesigns@gmail.com>
2023-02-22 01:10:39 +0200

    added delete message icon and functionality. Upon delete it reloads the window

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,36 +1,42 @@
-const Chat = require('../modules/chatModule');
+const Chat = require("../modules/chatModule");
 
-const postMessage= async (req, res) => {
-  const {sender, receiver,room, message} = req.body;
-    try {
-      let newMessage = new Chat ({
-        sender,
-        receiver,
-        room,
-        message
-      })
-      await newMessage.save();
-      res.status(200).send(newMessage);
-      } 
-     catch (error) {
-      res.status(500).json(error);
-    }
-  };
-
-  const getMessages = async (req, res) => {
-    try {
-      let messages = await Chat.find({})
-      .populate("sender")
-      .populate("receiver");
-      res.send(messages)
-    } catch (error) {
-      res.status(500).json(error);
-    }
+const postMessage = async (req, res) => {
+  const { sender, receiver, room, message } = req.body;
+  try {
+    let newMessage = new Chat({
+      sender,
+      receiver,
+      room,
+      message,
+    });
+    await newMessage.save();
+    res.status(200).send(newMessage);
+  } catch (error) {
+    res.status(500).json(error);
   }
+};
 
-  
-  module.exports = {
-    postMessage,
-    getMessages 
-  };
-  
+const getMessages = async (req, res) => {
+  try {
+    let messages = await Chat.find({}).populate("sender").populate("receiver");
+    res.send(messages);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const deleteMessage = async (req, res) => {
+  try {
+  await Chat.deleteOne({_id: req.params.id})
+  res.send({ message: "deleted" });
+} catch (error) {
+  res.status(500).json(error);
+}
+};
+
+
+module.exports = {
+  postMessage,
+  getMessages,
+  deleteMessage,
+};
