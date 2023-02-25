076e52cdabf6d46ed1b6f91ee79d243f711cbac6
Anita Angelidou <anitawebdesigns@gmail.com>
2023-02-26 00:52:49 +0200

    fixed the ui of the messages for the rest of the users, but doesn't display the pictures of other users.

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -104,31 +104,109 @@ function Message({ chat, connectedUser, onClick, picture }) {
             );
           } else {
             return (
+              // <Paper
+              //   key={chatmessage._id}
+              //   sx={{
+              //     display: "flex",
+              //     flexDirection: "column",
+              //     flexWrap: "wrap",
+              //     alignSelf: "flex-end",
+              //     width: "23vw",
+              //     my: 1,
+              //     p: 1,
+              //     // backgroundColor: "#E4A11B",
+              //     backgroundColor: "#8bc34a",
+              //     borderRadius: "10px",
+              //   }}
+              // >
+              //   <p>{chatmessage?.sender?.username}:</p>
+              //   <p>{chatmessage.message}</p>
+              //   <p>
+              //     {
+              //       <Moment format="DD/MM/YYYY HH:mm">
+              //         {chatmessage.createdAt}
+              //       </Moment>
+              //     }
+              //   </p>
+              // </Paper>
+
+
+<Box sx={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
+
               <Paper
-                key={chatmessage._id}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  flexWrap: "wrap",
-                  alignSelf: "flex-end",
-                  width: "23vw",
-                  my: 1,
-                  p: 1,
-                  // backgroundColor: "#E4A11B",
-                  backgroundColor: "#8bc34a",
-                  borderRadius: "10px",
-                }}
-              >
-                <p>{chatmessage?.sender?.username}:</p>
-                <p>{chatmessage.message}</p>
-                <p>
-                  {
-                    <Moment format="DD/MM/YYYY HH:mm">
-                      {chatmessage.createdAt}
-                    </Moment>
-                  }
-                </p>
-              </Paper>
+                  key={chatmessage._id}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignSelf: "flex-end",
+                    flexWrap: "wrap",
+                    width: "55%",
+                    mr: 1,
+                    my: 1,
+                    p: 1,
+                    backgroundColor: "#8bc34a",
+                    borderRadius: "30px",
+                  }}
+                >
+                  <Box
+                    sx={{
+                      width: "100%",
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "flex-start",
+                      height: "2rem",
+                      px: 2,
+                    }}
+                  >
+                    <Typography style={{ fontWeight: "bold" }}>{chatmessage?.sender?.username}</Typography>
+                    <Box
+                      sx={{
+                        width: "100%",
+                        display: "flex",
+                        justifyContent: "flex-end",
+                        alignItems: "center",
+                      }}
+                    >
+                      <AccessTimeIcon
+                        style={{ width: "1rem", marginRight: ".2rem" }}
+                      />
+                      <Typography>
+                        {<Moment fromNow>{chatmessage.createdAt}</Moment>}
+                      </Typography>
+                    </Box>
+                  </Box>
+                  <Box
+                    sx={{
+                      width: "100%",
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      px: 1,
+                      py: 2,
+                    }}
+                    style={{ borderTop: ".5px solid lightgrey" }}
+                  >
+                    <Typography>{chatmessage.message}</Typography>
+
+                    <IconButton
+                      onClick={() => {
+                        deleteMessage(chatmessage._id);
+                      }}
+                      aria-label="Delete message"
+                    >
+                      <DeleteOutlineOutlinedIcon
+                        style={{ alignSelf: "center" }}
+                      />
+                    </IconButton>
+                  </Box>
+                </Paper>
+                <Avatar
+                  alt="Profile picture with menu"
+                  src={ picture || profileImg}
+                  style={{ width: "3.5rem", height: "3.5rem" }}
+                />
+                </Box>
             );
           }
         })}
